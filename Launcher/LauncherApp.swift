ed4b91d1ca36cafc8028a4d637e2f85744372f1b
Jed Fox <git@jedfox.com>
2021-02-25 12:08:56 -0500

    Initial Commit

---
+++ Launcher/LauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LauncherApp.swift
+//  Launcher
+//
+//  Created by Jed Fox on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct LauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
